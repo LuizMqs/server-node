2500729f1b8bcfa49f99e31cae90c1a7ac00bbfb
luizMqs <luizantmarquesjr@gmail.com>
2024-04-07 00:06:30 -0300

    feat: adds error handling and routes to the application

---
+++ dist/routes/register-for-event.mjs
@@ -0,0 +1,8 @@
+import {
+  registerForEvent
+} from "../chunk-QJS23M44.mjs";
+import "../chunk-JRO4E4TH.mjs";
+import "../chunk-JV6GRE7Y.mjs";
+export {
+  registerForEvent
+};
--- dist/server.mjs
+++ dist/server.mjs
@@ -1,58 +1,69 @@
-import fastify from "fastify";
+import {
+  registerForEvent
+} from "./chunk-QJS23M44.mjs";
+import {
+  errorHandler
+} from "./chunk-EOXGF5FC.mjs";
+import {
+  checkIn
+} from "./chunk-EVF765BR.mjs";
+import {
+  createEvent
+} from "./chunk-TAZXGJ64.mjs";
+import "./chunk-U5RXYY2H.mjs";
+import {
+  getAttendeeBadge
+} from "./chunk-FPSPEEES.mjs";
+import {
+  getEventAttendees
+} from "./chunk-LVXKP34Z.mjs";
+import {
+  getEvent
+} from "./chunk-XV33RQJ7.mjs";
+import "./chunk-JRO4E4TH.mjs";
+import "./chunk-JV6GRE7Y.mjs";
 
+// src/server.ts
+import fastify from "fastify";
 import fastifySwagger from "@fastify/swagger";
 import fastifySwaggerUI from "@fastify/swagger-ui";
 import fastifyCors from "@fastify/cors";
-
 import {
   serializerCompiler,
   validatorCompiler,
-  jsonSchemaTransform,
+  jsonSchemaTransform
 } from "fastify-type-provider-zod";
-import { createEvent } from "./routes/create-event";
-import { registerForEvent } from "./routes/register-for-event";
-import { getEvent } from "./routes/get-event";
-import { getAttendeeBadge } from "./routes/get-attendee-badge";
-import { checkIn } from "./routes/check-in";
-import { getEventAttendees } from "./routes/get-event-attendees";
-import { errorHandler } from "./error-handler";
-
-export const app = fastify();
-
+var app = fastify();
 app.register(fastifyCors, {
-  origin: "*",
+  origin: "*"
 });
-
 app.register(fastifySwagger, {
   swagger: {
     consumes: ["application/json"],
     produces: ["application/json"],
     info: {
       title: "pass.in",
-      description:
-        "Especificacoes da API para o back-end da aplicacao pass.in construida durante o NLW Unite da Rocketseat.",
-      version: "1.0.0",
-    },
+      description: "Especificacoes da API para o back-end da aplicacao pass.in construida durante o NLW Unite da Rocketseat.",
+      version: "1.0.0"
+    }
   },
-  transform: jsonSchemaTransform,
+  transform: jsonSchemaTransform
 });
-
 app.register(fastifySwaggerUI, {
-  routePrefix: "/docs",
+  routePrefix: "/docs"
 });
-
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
-
 app.register(createEvent);
 app.register(registerForEvent);
 app.register(getEvent);
 app.register(getAttendeeBadge);
 app.register(checkIn);
 app.register(getEventAttendees);
-
 app.setErrorHandler(errorHandler);
-
 app.listen({ port: 3333, host: "0.0.0.0" }).then(() => {
   console.log("HTTP server running!");
 });
+export {
+  app
+};
